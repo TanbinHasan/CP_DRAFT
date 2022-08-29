965d2fceea453ad0573fc8153e1822a26700aaf3
TanbinHasan <tanvin.hasan74@gmail.com>
2022-08-30 01:02:20 +0600

    Update 1.cpp

--- .Languages/C++/1.cpp
+++ .Languages/C++/1.cpp
@@ -6,7 +6,48 @@ using namespace std;
 
 // #define MultipleCase       
 void Solve(int tc) {
-  
+  int n, q;
+  cin >> n >> q;
+  int MX = (1LL << 33) - 1;
+  vector<int> val(n + 1, 0);
+  vector<bool> mark(n + 1);
+  vector<vector<int>> adj(n + 1);
+  map<pair<int,int>, int> mpval;
+  for (int i = 0; i < q; ++i) {
+    int a, b, x;
+    cin >> a >> b >> x;
+    if (a == b) {
+      val[a] = x;
+      mark[a] = true;
+      continue;
+    }
+    if (a > b) swap(a, b);
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+    if (!mark[a]) val[a] |= x;
+    if (!mark[b]) val[b] |= x;
+    mpval[{a, b}] = mpval[{b, a}] = x;
+  }
+  for (int s = 1; s <= n; ++s) {
+    for (auto &c : adj[s]) {
+      val[s] &= mpval[{c, s}];
+      val[c] &= mpval[{c, s}];
+    }
+  }
+  for (int i = 32; i >= 0; --i) {
+    for (int s = 1; s <= n; ++s) {
+      if (mark[s]) continue;
+      if ((bool) (val[s] & (1LL << i)) == false) continue;
+      int cnt = 0;
+      for (auto &c : adj[s]) {
+        if (val[c] & (1LL << i)) ++cnt;
+      }
+      if (cnt == (int)adj[s].size()) val[s] ^= (1LL << i);
+    }
+  }
+  for (int i = 1; i <= n; ++i) {
+    cout << val[i] << " ";
+  }
 }
 
 int32_t main(void) {
